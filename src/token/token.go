55a75ada78e2e6937219256fca334a2d94a658bd
yantera <taiyakitabetaiyo@gmail.com>
2019-12-24 17:06:27 +0900

    new: add token files from chapter 1-2

---
+++ src/token/token.go
@@ -0,0 +1,33 @@
+package token
+
+type TokenType string
+
+type Token struct {
+	Type TokenType
+	Literal string
+}
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF = "EOF"
+
+	// 識別子 + リテラル
+	IDENT = "IDENT" // add, foobar, x, y, ...
+	INT = "INT"
+
+	// 演算子
+	ASSIGN = "="
+	PLUS = "+"
+
+	// デリミタ
+	COMMA = ","
+	SEMICOLON = ";"
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// キーワード
+	FUNCTION = "FUNCTION"
+	LET = "LET"
+)
